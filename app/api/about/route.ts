bab98f18326d64a1c7a8232afea59cc763f167a2
Adham Nasser <150140392+Adhamxiii@users.noreply.github.com>
2024-12-02 18:33:50 +0200

    fix errors

--- app/api/about/route.ts
+++ app/api/about/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
     const response = await fetch('https://grandy-vo-dashboard.vercel.app/api/about');
     const data = await response.json();
     return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+  } catch (e) {
+    return NextResponse.json({ error: 'Failed to fetch data', e }, { status: 500 });
   }
 }
