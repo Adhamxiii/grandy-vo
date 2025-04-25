2e0cad354cc19c95a95da321e68e1062d5d17e1f
Adhamxiii <adhamxiii22@gmail.com>
2025-04-26 02:20:59 +0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,69 +1,18 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "img.youtube.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  compress: true,
-  swcMinify: true,
-  reactStrictMode: true,
+const nextConfig: NextConfig = {
   images: {
-    domains: ['img.youtube.com', 'res.cloudinary.com'],
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-    optimizePackageImports: ['framer-motion', 'lucide-react'],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  webpack: (config: any, { dev, isServer }: { dev: any, isServer: any }) => {
-    // Only run in production client build
-    if (!dev && !isServer) {
-      // Enable tree shaking for dependencies
-      config.optimization.usedExports = true;
+};
 
-      // Split chunks more aggressively
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all',
-            priority: 10,
-          },
-          framerMotion: {
-            test: /[\\/]node_modules[\\/]framer-motion[\\/]/,
-            name: 'framer-motion',
-            chunks: 'all',
-            priority: 20,
-          },
-        },
-      };
-    }
-
-    return config;
-  },
-}
-
-module.exports = nextConfig
+export default nextConfig;
